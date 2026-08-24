9569386d09903f52c46a747316cf7026088846cf
datagranate <datagranate@gmx.com>
2026-08-24 13:19:25 +0000

    comment out Chroma

--- rag/streamlit_app.py
+++ rag/streamlit_app.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from langchain_chroma import Chroma
+# from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
